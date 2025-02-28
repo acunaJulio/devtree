3a5d03ccf548ae867bdb117ab3bd6b374c23edfc
acunaJulio <113699469+acunaJulio@users.noreply.github.com>
2025-02-27 23:57:04 -0500

    cors

--- back end/src/config/cors.ts
+++ back end/src/config/cors.ts
@@ -1,14 +1,15 @@
-import {CorsOptions} from 'cors';
+import { CorsOptions } from 'cors';
 
-
+const allowedOrigins = ["http://localhost:5173"];
 
 export const corsOptions: CorsOptions = {
-
-    origin: function(origin, callback) {
-      if(origin === 'http://localhost:5173'){
-        callback(null, true)
-      }else{
-        callback(new Error('No permitido por CORS'))
-      }
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error("No permitido por CORS"));
     }
-}
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow required headers
+};
